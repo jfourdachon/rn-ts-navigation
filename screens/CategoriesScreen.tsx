4233b59a37e03539083a80a45deae7a623996560
jfourdachon <jfourdachon@yahoo.com>
2021-02-25 23:56:40 +0100

    ad category datas

--- screens/CategoriesScreen.tsx
+++ screens/CategoriesScreen.tsx
@@ -1,19 +1,29 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button, FlatList, ViewBase } from 'react-native';
 import { DefaultNavigationProps } from '../types';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { CATEGORIES } from '../data/dummy-data'
+import Category from '../models/category';
 
 
 interface Props {
   navigation: DefaultNavigationProps<'Categories'>;
 }
 
+type ItemData = {
+    item: Category
+}
+
+
+const renderItem = (itemData: ItemData) => {
+    return (
+        <View style={styles.grid}><Text>{itemData.item.title}</Text></View>
+    )
+}
+
 const CategoriesScreen = ({navigation}: Props) => {
   return (
-    <SafeAreaView style={styles.screen}>
-      <Text>The categories screen</Text>
-      <Button title='Go to Details' onPress={() => {navigation.navigate('CategoryMeals')}} />
-    </SafeAreaView>
+   <FlatList keyExtractor={(item : Category) => item.id} numColumns={2} data={CATEGORIES} renderItem={renderItem} />
   );
 };
 
@@ -23,6 +33,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  grid: {
+      flex: 1,
+      margin: 15,
+      height: 150
+  }
 });
 
 export default CategoriesScreen;
