b3f517e98943d7f302268952addf86b27c1dee83
jfourdachon <jfourdachon@yahoo.com>
2021-03-05 19:52:05 +0100

    add fallback elem in category meals

--- screens/CategoryMealsScreen.tsx
+++ screens/CategoryMealsScreen.tsx
@@ -4,6 +4,8 @@ import { CATEGORIES } from '../data/dummy-data';
 import { NavigationStackScreenComponent } from 'react-navigation-stack';
 import MealList from '../components/MealList';
 import { ROOT_STATE } from '../store/combineReducers';
+import { StyleSheet, View } from 'react-native';
+import DefaultText from '../components/DefaultText';
 
 const CategoryMealsScreen: NavigationStackScreenComponent = ({ navigation }) => {
 
@@ -11,6 +13,16 @@ const CategoryMealsScreen: NavigationStackScreenComponent = ({ navigation }) => 
 
   const availableMeals = useSelector((state:  ROOT_STATE) => state.meals.filteredMeals)
   const displayedMeals = availableMeals.filter((meal) => meal.categoryIds.includes(catId));
+
+  console.log({displayedMeals})
+
+  if (displayedMeals.length === 0 || !displayedMeals) {
+    return (
+      <View style={styles.content}>
+        <DefaultText>No meals found, maybe check your filters?</DefaultText>
+      </View>
+    );
+  }
 
   return (
    <MealList listData={displayedMeals} navigation={navigation}/>
@@ -24,6 +36,15 @@ CategoryMealsScreen.navigationOptions = ({ navigation }) => {
     headerTitle: selectedCategory?.title,
   };
 };
+
+
+const styles = StyleSheet.create({
+    content: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
 
 
 export default CategoryMealsScreen;
